5d59289d0c04554ba9c7329514bef84c24837f08
Kishore Kammela <kishorekammela@gmail.com>
2025-05-05 16:57:24 +0530

    feat: add About, Contact, Pricing, and Products pages with basic content; implement Layout and Footer components; enhance Navbar with sidebar support

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,16 @@
+import { Navbar } from "@/components/Navbar/navbar";
+import { Footer } from "@/components/Footer/footer";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="flex-1 bg-[#F4F4F0]">
+        {children}
+      </div>
+      <Footer/>
+    </div>
+	);
+};
+
+export default Layout;
---
+++ src/components/NavSidebar/navSidebar.tsx
@@ -0,0 +1,54 @@
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import Link from 'next/link';
+interface NavbarItems {
+	href: string;
+	children: React.ReactNode;
+}
+
+interface NavSidebarProps {
+	items: NavbarItems[];
+	open: boolean;
+	onOpenChange: (open: boolean) => void;
+}
+
+export const NavSidebar = ({ items, open, onOpenChange }: NavSidebarProps) => {
+	return (
+		<Sheet open={open} onOpenChange={onOpenChange}>
+			<SheetContent side='left' className='p-0 transition-none'>
+				<SheetHeader className='p-4 border-b'>
+					<SheetTitle className='text-2xl font-bold'>Menu</SheetTitle>
+				</SheetHeader>
+				<ScrollArea className='flex flex-col overflow-y-auto h-full pb-2'>
+					{items.map((item) => (
+						<Link
+							key={item.href}
+							href={item.href}
+              onClick={() => onOpenChange(false)}
+							className='w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium'
+						>
+							{item.children}
+						</Link>
+					))}
+
+					<div className='border-t'>
+						<Link
+							href='/sign-in'
+              onClick={() => onOpenChange(false)}
+							className='w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium'
+						>
+							Login
+						</Link>
+						<Link
+							href='/sign-up'
+              onClick={() => onOpenChange(false)}
+							className='w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium'
+						>
+							Start Selling
+						</Link>
+					</div>
+				</ScrollArea>
+			</SheetContent>
+		</Sheet>
+	);
+};
